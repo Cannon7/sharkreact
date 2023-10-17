64b8c4a17b4b724297d808ecab1cbbcb5163e9a8
Ellaluvscheese <gal21029@byui.edu>
2023-10-17 09:54:33 -0600

    Co-authored-by: Emma-John <Emma-John@users.noreply.github.com> Co-authored-by: Odd-Dragon <Odd-Dragon@users.noreply.github.com> Co-authored-by: FinneganTheFine <FinneganTheFine@users.noreply.github.com>

---
+++ Shark/main/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function footer(){
+
+    return(
+        <>
+            <p id="currentYear"></p>
+            <p>Blue Squids Ink.</p>
+            <p>BYU-IDAHO</p>
+            <p id="lastModified"></p>
+        </>
+    );
+}
---
+++ Shark/main/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function header(){
+
+    return(
+        <>
+            <img src='https://placehold.co/60x40' ></img>
+            <h1>Shark </h1>
+            <h3>Fun Shark Logo Here</h3>
+        </>
+    );
+}
