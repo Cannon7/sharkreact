72c817fde1707647bbabc734ae8401a8ea21919a
Cannon7 <roa22002@byui.edu>
2023-12-06 23:46:53 -0700

    Paths for Ella.css and the style folder fixed

--- Shark/main/src/pages/home.js
+++ Shark/main/src/pages/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Nav from '../components/nav';
-import '../styles/ella.css';
+import '../styles/home.css';
 
 function MainPage() {
   return (
