eec08076d8bb46cd5835edb9ffbd96a749e2eb28
Kalina Warner <kalinamosher7@gmail.com>
2023-12-07 09:45:41 -0700

    Fixed nav!

--- Shark/main/src/components/nav.js
+++ Shark/main/src/components/nav.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 
 function Nav() {
-    const [isShown, setIsShown] = useState(false);
-  
-    const toggleMenu = () => {setIsShown(!isShown);};
-    return (
-      <nav className={`navigation ${isShown ? 'shown' : ''}`}>
-        <a className="ham" href="#" onClick={toggleMenu}>&#9776;</a>
-        <a href="./home">Home</a>
-        <a href="./about">About</a>
-        <a href="./contact">Contact</a>
-        <a href="./profile">Profile</a>
-        <a href="./mylends">My Lends</a>
-        <a href="./myhistory">History</a>
-        </nav>
-    );
-};
+  const [isResponsive, setIsResponsive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsResponsive(!isResponsive);
+  };
+
+  return (
+    <nav className={`navigation ${isResponsive ? 'responsive' : ''}`}>
+      <a className="ham" href="#" onClick={toggleMenu}>
+        &#9776;
+      </a>
+      <a href="./home">Home</a>
+      <a href="./about">About</a>
+      <a href="./contact">Contact</a>
+      <a href="./profile">Profile</a>
+      <a href="./mylends">My Lends</a>
+      <a href="./myhistory">History</a>
+    </nav>
+  );
+}
 
 export default Nav;
