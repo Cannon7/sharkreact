c0027f53cf368fe253fe92e81ff4d84b262963ab
Kalina Warner <kalinamosher7@gmail.com>
2023-12-07 09:42:41 -0700

    Fixed nav so at least it displays

--- Shark/main/src/components/nav.js
+++ Shark/main/src/components/nav.js
@@ -1,31 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Nav() {
-    return(
-        <nav class="navigation" >
-        {/*Added Hamburger Menu functionality, 
-        changed the list to a collection of nav 
-        elements to remove the bullet points*/}
-            <a class="ham" href="#">&#9776;</a>
-            <a href="./home">Home</a>
-            <a href="./about">About</a>
-            <a href="./contact">Contact</a>
-            <a href="./profile">Profile</a>
-            <a href="./mylends">My Lends</a>
-            <a href="./myhistory">History</a>
+    const [isShown, setIsShown] = useState(false);
+  
+    const toggleMenu = () => {setIsShown(!isShown);};
+    return (
+      <nav className={`navigation ${isShown ? 'shown' : ''}`}>
+        <a className="ham" href="#" onClick={toggleMenu}>&#9776;</a>
+        <a href="./home">Home</a>
+        <a href="./about">About</a>
+        <a href="./contact">Contact</a>
+        <a href="./profile">Profile</a>
+        <a href="./mylends">My Lends</a>
+        <a href="./myhistory">History</a>
         </nav>
     );
 };
-
-/*--Hamburger menu--*/
-const nav1 = document.querySelector('.navigation');
-nav1.addEventListener('click', ()=>
-{nav1.classList.toggle('shown')});
-
-const mainnav = document.querySelector('.navigation');
-const hambutton = document.querySelector('.ham');
-
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('responsive');})
 
 export default Nav;
